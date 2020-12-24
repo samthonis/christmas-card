969584b71220a8bf44c1214f7ef07c992b84cad7
Sam Thonis <samthonis@gmail.com>
2020-12-24 16:08:00 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const adArray = ["/ad_amazon.gif", "/adanim.gif", "addesignHwbanLB.gif", "alarmGPA.gif"];
+
+const getAd = () => {
+
+    let num = Math.floor(Math.random()*adArray.length)
+    return adArray[num];
+}
+let newAd = getAd();
+console.log(newAd);
+document.getElementById("ad").innerHTML = `<img src="/assets/${newAd}" alt="">`
